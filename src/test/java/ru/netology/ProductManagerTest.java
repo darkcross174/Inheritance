9fa1467c9de372576a2cb8d01c8113f47ae855ee
Alexey Andreev <dark-cross@rambler.ru>
2021-01-31 20:55:54 +0300

    Change files

--- src/test/java/ru/netology/ProductManagerTest.java
+++ src/test/java/ru/netology/ProductManagerTest.java
@@ -1,28 +1,35 @@
 package ru.netology;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
 import ru.netology.manager.ProductManager;
 import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductManagerTest {
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+    Book book1 = new Book(1, "Financier", 799, "Dreiser");
+    Book book2 = new Book(2, "The Richest Man in Babylon", 899, "George Samuel Clayson");
+    Book book3 = new Book(3, "The Jungle book", 999, "oseph Rudyard Kipling");
+    Book book4 = new Book(4, "Instruction for Iphone 12", 1299, "Non author");
 
-    @Test()
-    public void shouldSearchNameBook() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager();
-        Book book1 = new Book(1, "Financier", 799, "Dreiser");
-        Book book2 = new Book(2, "The Richest Man in Babylon", 899, "George Samuel Clayson");
-        Book book3 = new Book(3, "The Jungle book", 999, "Joseph Rudyard Kipling");
-        Book book4 = new Book(4, "Instruction for Iphone 12", 1299, "Non author");
+    @BeforeEach
+    public void setUp() {
         manager.add(book1);
         manager.add(book2);
         manager.add(book3);
         manager.add(book4);
+    }
+
+    @Test
+    public void shouldSearchNameBook() {
         Product[] actual = manager.searchBy("The Richest Man in Babylon");
         Product[] expected = new Product[]{book2};
         assertArrayEquals(expected, actual);
@@ -30,37 +37,18 @@ class ProductManagerTest {
 
     @Test
     public void shouldSearchAuthorBook() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager();
-        Book book1 = new Book(1, "Financier", 799, "Dreiser");
-        Book book2 = new Book(2, "The Richest Man in Babylon", 899, "George Samuel Clayson");
-        Book book3 = new Book(3, "The Jungle book", 999, "Joseph Rudyard Kipling");
-        Book book4 = new Book(4, "Instruction for Iphone 12", 1299, "Non author");
-        manager.add(book1);
-        manager.add(book2);
-        manager.add(book3);
-        manager.add(book4);
         Product[] actual = manager.searchBy("Dreiser");
         Product[] expected = new Product[]{book1};
         assertArrayEquals(expected, actual);
     }
 
     @Test
+    /*Falling test*/
     public void shouldSearchNameTwoBook() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager();
-        Book book1 = new Book(1, "Financier", 799, "Dreiser");
-        Book book2 = new Book(2, "The Richest Man in Babylon", 899, "George Samuel Clayson");
-        Book book3 = new Book(3, "Financier", 999, "Non author");
-        Book book4 = new Book(4, "Instruction for Iphone 12", 1299, "Non author");
-        manager.add(book1);
-        manager.add(book2);
-        manager.add(book3);
-        manager.add(book4);
         Product[] actual = manager.searchBy("Financier");
         Product[] expected = new Product[]{book1, book3};
         assertArrayEquals(expected, actual);
     }
-
 }
+
 
