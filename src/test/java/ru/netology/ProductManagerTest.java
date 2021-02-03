1c0e129af0e4f1129df121d39ec736327244b505
Cliffart <Cliffart44@gmail.com>
2021-02-03 14:42:14 +0300

    Double search added

--- src/test/java/ru/netology/ProductManagerTest.java
+++ src/test/java/ru/netology/ProductManagerTest.java
@@ -16,8 +16,10 @@ class ProductManagerTest {
     ProductManager manager = new ProductManager(repository);
     Book book1 = new Book(1, "Financier", 799, "Dreiser");
     Book book2 = new Book(2, "The Richest Man in Babylon", 899, "George Samuel Clayson");
-    Book book3 = new Book(3, "Financier", 999, "Joseph Rudyard Kipling");
+    Book book3 = new Book(3, "Book of Jungle", 999, "Joseph Rudyard Kipling");
     Book book4 = new Book(4, "Instruction for Iphone 12", 1299, "Non author");
+    Book book5 = new Book(5, "Financier or not", 799, "Dreiser");
+    Book book6 = new Book(6, "Financier", 799, "Dreiser or not");
 
     @BeforeEach
     public void setUp() {
@@ -25,6 +27,8 @@ class ProductManagerTest {
         manager.add(book2);
         manager.add(book3);
         manager.add(book4);
+        manager.add(book5);
+        manager.add(book6);
     }
 
     @Test()
@@ -37,14 +41,14 @@ class ProductManagerTest {
     @Test
     public void shouldSearchAuthorBook() {
         Product[] actual = manager.searchBy("Dreiser");
-        Product[] expected = new Product[]{book1};
+        Product[] expected = new Product[]{book1, book5};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void shouldSearchNameTwoBook() {
         Product[] actual = manager.searchBy("Financier");
-        Product[] expected = new Product[]{book1, book3};
+        Product[] expected = new Product[]{book1, book6};
         assertArrayEquals(expected, actual);
     }
 }
